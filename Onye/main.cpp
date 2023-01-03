d474d21c90cfe595025288f91ea41fdf8eab0d40
Leonardo de Sousa <leonardo_ssl@hotmail.com>
2023-01-03 16:08:57 -0300

    Commit Inicial

---
+++ Onye/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstdio>
+#include <iostream> 
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    printf("olá do %s!\n", "Onye");
+    cout << argv[1] << endl;
+
+    return 0;
+}
